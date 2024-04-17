e3c3ed9bc23ff5ea371d5c94d1f275018704788f
Subash Ganesan <subash@pdx.edu>
2024-04-16 17:30:09 -0700

    Completed all the 24 levels of Flexbox Froggy and wrote all the commands in flexbox.md file. Completed the palindrome code and modified the palindrom.js file by addind necessary checks and validating the input.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,37 @@
-const elem = document.querySelector('input');
+function handleInput() {
+  resultElem.textContent = "";
+  resultElem.className = "";
 
-elem.addEventListener('input', handleInput);
+  const numStr = inputElem.value.trim();
+
+  if (numStr === "" || isNaN(numStr)) {
+    resultElem.textContent = "Please enter a number.";
+    resultElem.className = "text-danger";
+    return;
+  }
+
+  const num = parseFloat(numStr);
+
+  if (num < 0 || isNaN(num)) {
+    resultElem.textContent = "Please enter a positive number.";
+    resultElem.className = "text-danger";
+    return;
+  }
+
+  if (isPalindrome(numStr)) {
+    resultElem.textContent = "Yes. This is a palindrome!";
+    resultElem.className = "text-success";
+  } else {
+    resultElem.textContent = "No. Try again.";
+    resultElem.className = "text-danger";
+  }
+}
+
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
+}
+
+const inputElem = document.getElementById("positiveNumber");
+const resultElem = document.getElementById("palindromeResult");
+
+inputElem.addEventListener("input", handleInput);
